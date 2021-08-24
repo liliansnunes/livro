00a00ef271c9abbbbf507879d48656c387a236e8
liliansnunes <liliansnunes@outlook.com.br>
2021-08-24 18:53:50 +0000

    atualização dos pacotes de criação

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -2,7 +2,10 @@ package application;
 
 public class Application {
     public static void main(String args[]){
-        
+
+        // git add.
+        // git commit -m "ciração dos pacotes"
+        // git push
     }
 
 }
